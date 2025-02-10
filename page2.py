b43a421a092137fd5335d9f350b3cca393b837e3
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-10 16:11:38 +0700

    Delete page2.py

--- page2.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def app():
-    st.write("Welcome to Page 2!")
-    st.write("This is Page 2. You can customize this page with additional features or content.")
-    # Tambahkan konten lainnya untuk halaman 2 sesuai kebutuhan
