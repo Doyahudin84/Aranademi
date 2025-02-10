0e5c857da2c7e5cb44fb7df6b976462f750058c1
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-10 15:55:38 +0700

    home.py

---
+++ home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def app():
+    st.write("Welcome to the Home Page!")
+    st.write("This is where you can introduce your application and provide some general information.")
+    # Tambahkan konten lainnya untuk halaman utama sesuai kebutuhan
