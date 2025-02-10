92959a97360f5fc552549ef773467276aad23bcb
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-10 15:56:31 +0700

    page2.py

---
+++ page2.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def app():
+    st.write("Welcome to Page 2!")
+    st.write("This is Page 2. You can customize this page with additional features or content.")
+    # Tambahkan konten lainnya untuk halaman 2 sesuai kebutuhan
