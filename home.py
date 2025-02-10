b73d187543d89a84ac845d78c163f3ce8650ffea
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-10 16:11:14 +0700

    Delete home.py

--- home.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def app():
-    st.write("Welcome to the Home Page!")
-    st.write("This is where you can introduce your application and provide some general information.")
-    # Tambahkan konten lainnya untuk halaman utama sesuai kebutuhan
