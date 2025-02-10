7c7cf87f8fa2b258ab6ded008441fa252db25885
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-10 15:56:07 +0700

    page1.py

---
+++ page1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def app():
+    st.write("Welcome to Page 1!")
+    st.write("Here you can add the content for Page 1. This could be a detailed explanation or something interactive.")
+    # Tambahkan konten lainnya untuk halaman 1 sesuai kebutuhan
