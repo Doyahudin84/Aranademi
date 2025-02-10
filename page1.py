35207eedacb815038e6c01a846b67f2582e9193f
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-10 16:11:26 +0700

    Delete page1.py

--- page1.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def app():
-    st.write("Welcome to Page 1!")
-    st.write("Here you can add the content for Page 1. This could be a detailed explanation or something interactive.")
-    # Tambahkan konten lainnya untuk halaman 1 sesuai kebutuhan
