e298c1a9bdd9dcea35038250014a32ef346cece8
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-11 16:45:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import importlib
+from pages import *
 
 # Set the page configuration
 st.set_page_config(layout="centered", initial_sidebar_state="collapsed")
