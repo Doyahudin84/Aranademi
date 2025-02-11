e9b7e5b7d995e2eb266f1ca07b72597395e96af9
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-11 15:12:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ from streamlit_option_menu import option_menu
 from pages import *
 
 # Set page configuration to always be mobile-sized
-st.set_page_config(layout="centered", initial_sidebar_state="empty")
+st.set_page_config(layout="centered")
+st.sidebar.empty()
 class MultiApp:
 
     def __init__(self):
