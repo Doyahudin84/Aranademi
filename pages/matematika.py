877bf863457ae8b95e69760ca0ab16718dd0778b
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-11 16:50:23 +0700

    Update matematika.py

--- pages/matematika.py
+++ pages/matematika.py
@@ -1,1 +1,7 @@
+import streamlit as st
+
+def show_page():
+    st.title("Matematika")
+    st.write("Welcome to the Matematika page!")
+    st.write("Here we will discuss mathematical concepts.")
 
