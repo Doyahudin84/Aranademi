576f1b48aabaaeeeb37996f542b2836fa44d1c89
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-11 13:17:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
-from pages import home
+from streamlit_option_menu import option_menu
+from pages import *
 # Set page configuration to always be mobile-sized
 st.set_page_config(layout="centered", initial_sidebar_state="collapsed")
