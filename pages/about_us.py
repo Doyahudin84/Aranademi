36f2927dd167b1819bf8f7ad7e21e7f0457b93e9
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-11 16:45:02 +0700

    Update and rename aboutus.py to about_us.py

---
+++ pages/about_us.py
@@ -0,0 +1,8 @@
+# pages/aboutus.py
+import streamlit as st
+
+def show_page():
+    st.title("About Us")
+    st.write("This is the About Us page of the app.")
+
+
--- pages/aboutus.py
+++
@@ -1,8 +0,0 @@
-# pages/aboutus.py
-
-import streamlit as st
-
-def doyaapp():
-    st.title("Halaman Home")
-    st.write("Selamat datang di halaman utama aplikasi Streamlit.")
-
