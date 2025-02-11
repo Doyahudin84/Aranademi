aac9da873c248cbac57685ab7464ddea6ff1240e
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-11 17:28:06 +0700

    Update matematika.py

--- pages/matematika.py
+++ pages/matematika.py
@@ -3,16 +3,5 @@ import streamlit as st
 def show_page():
     st.title("Matematika")
     st.write("Welcome to the Matematika page!")
-    st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt tempus ligula, sed gravida urna sodales ut. 
-Pellentesque at tortor ut justo dapibus tristique. Mauris tincidunt enim ut metus dignissim, a sollicitudin orci fermentum. 
-Proin fermentum ligula ut magna dictum, sed auctor elit lacinia. Nam euismod, lorem sit amet gravida congue, lorem leo suscipit ligula, 
-eu fermentum ante dui non lectus. Curabitur id volutpat libero. Integer ac tincidunt ante, a lobortis justo.
-
-Integer suscipit dolor id nulla tincidunt, vel lacinia enim suscipit. Curabitur ut libero sit amet arcu vestibulum feugiat non nec arcu. 
-Vivamus vestibulum, ante vitae ultricies vehicula, sem neque lobortis magna, non volutpat enim elit ac metus. Aenean posuere felis ac 
-metus scelerisque, id tristique enim malesuada. Aliquam quis lorem ac dolor tincidunt auctor. Nulla nec tempor augue, non scelerisque eros.
-
-Sed ut aliquam felis, id volutpat orci. Integer tincidunt purus ac orci scelerisque, at varius tortor lacinia. Sed lobortis purus sit amet 
-erat fermentum, a dictum arcu suscipit. Donec convallis, lorem at posuere fermentum, velit sem gravida mi, sed tristique arcu purus id elit. 
-Nam ac magna vel odio vulputate feugiat non sed arcu. Donec sed placerat purus, ac facilisis sem.")
+    st.write("hehehehehehehehehehehehehehehe.")
 
