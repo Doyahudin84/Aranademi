60f2ac4b12f0c7fea066e5a799e95e23ab76c895
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-12 10:34:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ with st.sidebar:
     selected = option_menu(
         "Aranademi",
         ["About Us", "Matematika", "Fisika", "Kimia", "Biologi", "Donasi"],
-        icons=["caret-right-fill", "caret-right-fill", "caret-right-fill", "caret-right-fill", "caret-right-fill", "donate"],
+        icons=["caret-right-fill", "caret-right-fill", "caret-right-fill", "caret-right-fill", "caret-right-fill", "cash-coin"],
         menu_icon="cast",
         default_index=0,
        
