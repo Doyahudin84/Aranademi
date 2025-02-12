b6868e3a7470f3ce5a899df73faa811f0e83d59b
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-12 09:15:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ with st.sidebar:
     )
 
 selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    icons=['house', 'cloud-upload', "atom", 'gear'], 
     menu_icon="cast", default_index=0, orientation="horizontal")
 selected2
 
