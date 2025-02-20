86da1381d4831c0ceb90fca823338c7132b16020
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-20 18:32:09 +0700

    Update donasi.py

--- pages/donasi.py
+++ pages/donasi.py
@@ -1,7 +1,10 @@
 import streamlit as st
 
 def show_page():
+    url ='https://saweria.co/AranademiApp'
     st.title("Dukungan Anda")
     st.write("Dukungan anda kami perlukan!")
     st.write("Here we will discuss donasi concepts.")
+    st.markdown(f'<a href="{url}" target="_blank" style="background-color: #008CBA; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 4px;">Donate</a>', unsafe_allow_html=True)
+
 
